c5ee39cc9fc168b1221a4e6e7f2b8166096cfbe4
Kevin Yuan <kyuan191787@gmail.com>
2022-07-26 16:57:51 -0700

    oops forgot to commit these

---
+++ MusicAppBackend/src/test/java/com/kyuan/MusicAppBackend/MusicAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyuan.MusicAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
